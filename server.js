88e02891f515c98cea2cf6819f86b4f2c68c8ebc
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-10-12 16:05:29 -0300

    Mongoose connect error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.CONNECTSTRING, {
     useFindAndModify: false })
 .then(() => {
         app.emit('connect')
-}).catch(e => console.log('Mongoose connect error \n\n\n\n\n', e, '\n\n\n'));
+}).catch(e => console.log('Mongoose connect error'));
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
